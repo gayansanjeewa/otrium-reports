f731a5769990abc8ac59f2ed81344c0c3ff34f32
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-16 01:58:22 +0530

    install twig

--- app/config.php
+++ app/config.php
@@ -1,5 +1,10 @@
 <?php
 
-return [
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
+return [
+    Environment::class => function () {
+        return new Environment(new FilesystemLoader(__DIR__ . '/../src/Views'));
+    },
 ];
