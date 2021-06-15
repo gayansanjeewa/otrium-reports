e3e0f28c359f3a560407c98dd34dcd56caa02082
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-16 01:58:22 +0530

    install twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,27 @@
 
 namespace App\Controller;
 
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
 class HomeController
 {
+    private Environment $twig;
+
+    public function __construct(Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws LoaderError
+     */
     public function __invoke()
     {
-        echo "Hello, World!";
+        echo $this->twig->render('home.twig');
     }
 }
