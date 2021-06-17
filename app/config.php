90897269b85a2637882ad620ab19d8713726a9d2
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-18 03:10:42 +0530

    install dbal and refactor

--- app/config.php
+++ app/config.php
@@ -1,10 +1,33 @@
 <?php
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
+use Psr\Container\ContainerInterface;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
 return [
+    'connection' => [
+        'url' => sprintf(
+            'mysql://%s:%s@%s:%s/%s',
+            DI\env('MYSQL_USER'),
+            DI\env('MYSQL_PASSWORD'),
+            DI\env('DATABASE_HOST'),
+            DI\env('DATABASE_PORT'),
+            DI\env('DATABASE_NAME')
+        )
+    ],
+
+    // Twig
     Environment::class => function () {
-        return new Environment(new FilesystemLoader(__DIR__ . '/../src/Views'));
+        $loader = new FilesystemLoader(__DIR__ . '/src/Views');
+        return new Environment($loader, [
+            'debug' => true,
+        ]);
+    },
+
+    // Database
+    Connection::class => function (ContainerInterface $c) {
+        return DriverManager::getConnection($c->get('connection.url'));
     },
 ];
