12adc1632662b697a40b6a3b640af8cc2318a565
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-19 11:18:30 +0530

    wip - create report

---
+++ src/Repository/GMVRepository.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Repository;
+
+use App\Repository\Contract\GMVRepositoryInterface;
+use Doctrine\DBAL\Connection;
+use League\Csv\Writer;
+use Psr\Container\ContainerInterface;
+use SplTempFileObject;
+use function DI\create;
+
+class GMVRepository implements GMVRepositoryInterface
+{
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getAll()
+    {
+        $sql = "
+            select date as Day, b.name as 'Brand Name', sum(turnover * (1-.21)) as 'Turnover Excluding Vat'
+            from gmv
+            join brands b on b.id = gmv.brand_id
+            where (date between '2018-05-01' and '2018-05-07')
+            group by name, date
+            order by date
+            ";
+
+        $stmt = $this->connection->prepare($sql);
+//        $stmt->bindValue("name", $name);
+        $resultSet = $stmt->executeQuery();
+
+
+//        $writer->insertAll(new ArrayIterator($records));
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \Doctrine\DBAL\Driver\Exception
+     */
+    public function getSevenDayTurnoverPerBrand(string $startDate, string $endDate, float $vat): array
+    {
+
+        $sql = "
+            select date as Day, b.name as 'Brand Name', sum(turnover * :vat_deduction) as 'Turnover Excluding Vat'
+            from gmv
+            join brands b on b.id = gmv.brand_id
+            where (date between :start and :end)
+            group by name, date
+            order by date
+            ";
+
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindValue(':start', $startDate);
+        $stmt->bindValue(':end', $endDate);
+        $stmt->bindValue(':vat_deduction', 1- $vat);
+
+        $resultSet = $stmt->executeQuery();
+
+        return $resultSet->fetchAllAssociative();
+    }
+}
