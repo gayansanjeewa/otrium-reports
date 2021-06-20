3d5bdb2d39da2c1e342bf26cdebb18c018969a74
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-20 16:54:40 +0530

    refactor

--- src/Repository/Contract/GMVRepositoryInterface.php
+++ src/Repository/Contract/GMVRepositoryInterface.php
@@ -2,9 +2,29 @@
 
 namespace App\Repository\Contract;
 
+use App\Exception\NotFoundHttpException;
+
 interface GMVRepositoryInterface
 {
+    /**
+     * @param string $startDate
+     * @param string $endDate
+     * @param float $vat
+     * @return array
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \Doctrine\DBAL\Driver\Exception
+     * @throws NotFoundHttpException
+     */
     public function getSevenDayTurnoverPerBrand(string $startDate, string $endDate, float $vat): array;
 
+    /**
+     * @param string $startDate
+     * @param string $endDate
+     * @param float $vat
+     * @return array
+     * @throws \Doctrine\DBAL\Exception
+     * @throws \Doctrine\DBAL\Driver\Exception
+     * @throws NotFoundHttpException
+     */
     public function getSevenDayTurnoverPerDay(string $startDate, string $endDate, float $vat): array;
 }
