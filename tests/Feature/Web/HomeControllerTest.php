3ac295df301766110f7cd7e5ba1bc6c20bc2d451
Gayan Sanjeewa <iamgayan@gmail.com>
2021-06-21 10:50:50 +0530

    --wip-- [skip ci]

---
+++ tests/Feature/Web/HomeControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Web;
+
+use PHPUnit\Framework\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    /**
+    * @test
+    */
+    public function invoke_shouldServeHomePage()
+    {
+        // TODO@Gayan: todo
+    }
+}
